f71c4bda854336cb295a22ae73f4cf1368f26c31
kyo-kbys <kyokoba1007@gmail.com>
2021-05-07 01:19:01 +0900

    Add make env for only agent mode

--- make_env.py
+++ make_env.py
@@ -4,8 +4,11 @@ def make_env(env_name):
     env = make(env_name, debug=True)
     return env
 
-def make_trainer(env_name='hungry_geese', players=["random"]):
+def make_trainer(env_name='hungry_geese', enemy_players=["random"]):
     env = make_env(env_name)
     # Make trainer for kaggle env agent
-    trainer = env.train([None] + players)
+    if enemy_players is None:
+        trainer = env.train([None]) # Only agent
+    else:
+        trainer = env.train([None] + players) # with enemy
     return trainer
